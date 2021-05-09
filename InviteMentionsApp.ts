68f2121bc49730713e280b24ce65b5a620435fb5
yash-rajpal <rajpal.yash03@gmail.com>
2021-05-09 21:52:16 +0530

    extending settings

--- InviteMentionsApp.ts
+++ InviteMentionsApp.ts
@@ -1,3 +1,13 @@
+import { IAppAccessors, IConfigurationExtend, IEnvironmentRead, ILogger } from '@rocket.chat/apps-engine/definition/accessors';
 import { App } from '@rocket.chat/apps-engine/definition/App';
+import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
+import { settings } from './src/config/Settings';
 
-export default class InviteMentionsApp extends App {}
+export default class InviteMentionsApp extends App {
+    constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
+        super(info, logger, accessors);
+    }
+    protected async extendConfiguration(configuration: IConfigurationExtend, environmentRead: IEnvironmentRead): Promise<void> {
+        await Promise.all(settings.map((setting) => configuration.settings.provideSetting(setting)));
+    }
+}
