f5ed11245ac69ab7aa28e51a26405646558baafd
yash-rajpal <58601732+yash-rajpal@users.noreply.github.com>
2021-05-09 22:44:31 +0530

    Adding basic settings to app (#7)
    * adding basic settings
    
    * extending settings
    
    * removing unused var
    
    * changing default vals to True
    
    * lint
    
    * correct eslint
    
    Co-authored-by: aditya-mitra <ultrapokeinstinct@gmail.com>

--- InviteMentionsApp.ts
+++ InviteMentionsApp.ts
@@ -1,3 +1,11 @@
+import { IConfigurationExtend } from '@rocket.chat/apps-engine/definition/accessors';
 import { App } from '@rocket.chat/apps-engine/definition/App';
+import { settings } from './src/config/Settings';
 
-export default class InviteMentionsApp extends App {}
+export default class InviteMentionsApp extends App {
+	protected async extendConfiguration(configuration: IConfigurationExtend): Promise<void> {
+		await Promise.all(
+			settings.map((setting) => configuration.settings.provideSetting(setting))
+		);
+	}
+}
