9b4afba2e73f2c4a1a07f5309d69db4ee8da0cb3
yash-rajpal <rajpal.yash03@gmail.com>
2021-05-12 18:04:47 +0530

    lint

--- InviteMentionsApp.ts
+++ InviteMentionsApp.ts
@@ -1,15 +1,17 @@
-import { IAppAccessors, IConfigurationExtend, IHttp, ILogger, IModify, IPersistence, IRead } from '@rocket.chat/apps-engine/definition/accessors';
+import {
+	IConfigurationExtend,
+	IHttp,
+	IModify,
+	IPersistence,
+	IRead
+} from '@rocket.chat/apps-engine/definition/accessors';
 import { App } from '@rocket.chat/apps-engine/definition/App';
 import { IMessage, IPostMessageSent } from '@rocket.chat/apps-engine/definition/messages';
-import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
 import { settings } from './src/config/Settings';
 import TestCommand from './src/config/SlashCommands';
-import { PostMessageSentHandler } from './src/handlers/PostMessageSentHandler'
-export default class InviteMentionsApp extends App implements IPostMessageSent {
-	constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
-        super(info, logger, accessors);
-    }
+import PostMessageSentHandler from './src/handlers/PostMessageSentHandler';
 
+export default class InviteMentionsApp extends App implements IPostMessageSent {
 	async executePostMessageSent(
 		message: IMessage,
 		read: IRead,
--- src/handlers/PostMessageSentHandler.ts
+++ src/handlers/PostMessageSentHandler.ts
@@ -3,36 +3,35 @@ import { IApp } from '@rocket.chat/apps-engine/definition/IApp';
 import { IMessage } from '@rocket.chat/apps-engine/definition/messages';
 import SendInviteMessage from '../lib/sendInviteMessage';
 
-export class PostMessageSentHandler {
+export default class PostMessageSentHandler {
 	constructor(
 		private app: IApp,
 		private message: IMessage,
 		private read: IRead,
 		private modify: IModify
-	) {}
+	) {
+		this.message = message;
+	}
 
-	public async run() {
+	public async run(): Promise<void> {
 		const messageText = this.message.text || '';
-		const sender = this.message.sender;
-		const room = this.message.room;
-		var roomMembers = await this.read.getRoomReader().getMembers(room.id);
+		const { sender, room } = this.message;
+		const roomMembers = await this.read.getRoomReader().getMembers(room.id);
 		const roomMemberUserNames = roomMembers.map((roomMember) => roomMember.username);
-		var mentionsRegex = new RegExp('@([a-zA-Z0-9_.]+)', 'gim');
-		var allMentions = messageText.match(mentionsRegex);
+		const mentionsRegex = new RegExp('@([a-zA-Z0-9_.]+)', 'gim');
+		let allMentions = messageText.match(mentionsRegex);
 		if (allMentions && allMentions.length) {
 			allMentions = allMentions.map((match) => match.slice(1));
 			allMentions.map(async (userName) => {
-				let user = await this.read.getUserReader().getByUsername(userName);
+				const user = await this.read.getUserReader().getByUsername(userName);
 				if (roomMemberUserNames.includes(user.username) === false) {
-					console.log('User Not in Room', user.username);
-					const sendInvite = new SendInviteMessage({
+					new SendInviteMessage({
 						mentionedUser: user,
 						receivingUser: sender,
 						block: this.modify.getCreator().getBlockBuilder(),
 						notify: this.modify.getNotifier(),
-						room: room
-					});
-					sendInvite.sendMessage();
+						room
+					}).sendMessage();
 				}
 			});
 		}
