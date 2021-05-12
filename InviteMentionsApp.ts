4e448ec4e0788e1361bed9ca8ad1c184355a2b69
yash-rajpal <rajpal.yash03@gmail.com>
2021-05-12 15:56:10 +0530

    post message and mentions

--- InviteMentionsApp.ts
+++ InviteMentionsApp.ts
@@ -1,10 +1,23 @@
-import { IConfigurationExtend } from '@rocket.chat/apps-engine/definition/accessors';
+import { IAppAccessors, IConfigurationExtend, ILogger, IRead } from '@rocket.chat/apps-engine/definition/accessors';
 import { App } from '@rocket.chat/apps-engine/definition/App';
-
+import { IMessage, IPostMessageSent } from '@rocket.chat/apps-engine/definition/messages';
+import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
 import { settings } from './src/config/Settings';
 import TestCommand from './src/config/SlashCommands';
+import { PostMessageSentHandler } from './src/handlers/PostMessageSentHandler'
+export default class InviteMentionsApp extends App implements IPostMessageSent {
+	constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
+        super(info, logger, accessors);
+    }
 
-export default class InviteMentionsApp extends App {
+	async executePostMessageSent(
+		message: IMessage,
+		read: IRead
+	): Promise<void> {
+		const postMessageHandler = new PostMessageSentHandler(this, message, read);
+		postMessageHandler.run();
+	}
+
 	protected async extendConfiguration(configuration: IConfigurationExtend): Promise<void> {
 		await Promise.all(
 			settings.map((setting) => configuration.settings.provideSetting(setting))
---
+++ src/handlers/PostMessageSentHandler.ts
@@ -0,0 +1,18 @@
+import { IRead } from '@rocket.chat/apps-engine/definition/accessors';
+import { IApp } from '@rocket.chat/apps-engine/definition/IApp';
+import { IMessage } from '@rocket.chat/apps-engine/definition/messages';
+
+export class PostMessageSentHandler {
+	constructor(private app: IApp, private message: IMessage, private read: IRead) {}
+
+	public async run() {
+		const messageText = this.message.text || '';
+		var mentionsRegex = new RegExp('@([a-zA-Z0-9\_\.]+)', 'gim');
+		var allMentions = messageText.match(mentionsRegex);
+		if (allMentions && allMentions.length) {
+			allMentions = allMentions.map((match)=> match.slice(1));
+			
+		}
+		this.app.getLogger().debug("testing" , allMentions ? allMentions : 'Null') // If NULL is given to LOGS app crashed so checking
+	}
+}
