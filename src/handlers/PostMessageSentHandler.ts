27c6d564e08794a3c755dd28cc52a515a219fe92
yash-rajpal <rajpal.yash03@gmail.com>
2021-05-12 17:40:01 +0530

    checking room members

--- src/handlers/PostMessageSentHandler.ts
+++ src/handlers/PostMessageSentHandler.ts
@@ -7,14 +7,19 @@ export class PostMessageSentHandler {
 
 	public async run() {
 		const messageText = this.message.text || '';
+		const room = this.message.room;
+		var roomMembers = await this.read.getRoomReader().getMembers(room.id);
+		const roomMemberUserNames = roomMembers.map((roomMember) => roomMember.username);
 		var mentionsRegex = new RegExp('@([a-zA-Z0-9\_\.]+)', 'gim');
 		var allMentions = messageText.match(mentionsRegex);
 		if (allMentions && allMentions.length) {
 			allMentions = allMentions.map((match)=> match.slice(1));
-			allMentions.map((userName) => {
-				this.read.getUserReader().getByUsername(userName).then((res) => console.log("details", res))
+			allMentions.map(async (userName) => {
+				let user = await this.read.getUserReader().getByUsername(userName);
+				if(roomMemberUserNames.includes(user.username) === false){
+					console.log("User Not in Room", user.username)
+				}
 			})
 		}
-		this.app.getLogger().debug("testing" , allMentions ? allMentions : 'Null') // If NULL is given to LOGS app crashed so checking
 	}
 }
